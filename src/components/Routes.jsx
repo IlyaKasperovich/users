ace6028c600a0b77d3bbaee268ca9e9fd3b5a358
Ilya Kasperovich <kasper.ii2108@gmail.com>
2021-04-16 10:25:50 +0200

    users-3 resolves conflicts between eslint and prettier

--- src/components/Routes.jsx
+++ src/components/Routes.jsx
@@ -1,10 +1,5 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Redirect,
-} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
 import LoginForm from './LoginForm';
 import UserForm from './UserForm';
 import UsersTable from './UsersTable';
